bdf58c3aa8f6c88bd6489a86bbad64d768a42645
Anjan Nepal <rockanjan@gmail.com>
2013-04-12 12:22:48 -0400

    modified settings file

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,5 @@
+ROOT_FOLDER = '/home/anjan/workspace/AclAnthologyMining/src/'
 DATA_FOLDER = '/home/anjan/data/acl_anthology/aan/'
 PAPERS_FOLDER = DATA_FOLDER + 'papers_text/'
-STOP_WORD_FILE = DATA_FOLDER + "/english.stop.txt"
+STOP_WORD_FILE = ROOT_FOLDER + "/english.stop.txt"
 VOCAB_FILE = DATA_FOLDER + "vocab.txt"
